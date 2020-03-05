108deb0880bc921a68995549da493a1d52f59822
Sagar Verma <sagar15056@iiitd.ac.in>
2020-03-05 17:27:28 +0100

    integral testing working

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -10,4 +10,20 @@ class Test_Experiment(object):
 
         assert isinstance(experiment.config, ExperimentConfig)
         assert isinstance(experiment, Experiment)
-        
+
+    def test__integral(self):
+        config = ExperimentConfig(integral=True, simulate=False)
+        experiment = Experiment(config)
+
+        assert experiment.torque_states >= config.static_states[0] and\
+               experiment.torque_states <= config.static_states[1]
+        assert experiment.speed_states >= config.static_states[0] and\
+                experiment.torque_states <= config.static_states[1]
+
+        for torque in experiment.reference_torque:
+            assert isinstance(torque, int)
+            assert torque >= config.torque_range[0] and torque <= config.torque_range[1]
+
+        for speed in experiment.reference_speed:
+            assert isinstance(speed, int)
+            assert speed >= config.speed_range[0] and speed <= config.speed_range[1]
