ef19ad9d59e27cf1932b78a8a92c5ab7b942e581
Sagar <sagar15056@iiitd.ac.in>
2020-03-06 17:22:09 +0100

    comments in experiment.py added

--- motorrefgen/experiment.py
+++ motorrefgen/experiment.py
@@ -2,6 +2,11 @@ import random
 
 class Experiment(object):
     def __init__(self, config):
+        """
+        This class is used to generate reference speed and reference load
+        trajectories, can also be used to simulate those trajectories using
+        simulink model.
+        """
         self.config = config
 
         self.torque_states = random.randint(*config.static_states)
@@ -20,6 +25,9 @@ class Experiment(object):
             self.torque_time[-1] = self.speed_time[-1]
 
     def _get_ramp(self):
+        """
+        Get a ramp with random slope.
+        """
         if self.config.integral:
             ramp = random.choice(self.config.ramps)
         else:
@@ -28,6 +36,9 @@ class Experiment(object):
         return ramp
 
     def _integral_trajectory(self, points):
+        """
+        Generate a simple trajectory with integral values.
+        """
         time = [0]
         reference = []
 
@@ -43,6 +54,9 @@ class Experiment(object):
         return time, reference
 
     def _continuous_trajectory(self, points):
+        """
+        Generate a random trajectory from a continuous distribution.
+        """
         time = [0]
         reference = []
 
@@ -63,20 +77,32 @@ class Experiment(object):
         return time, reference
 
     def _gen_integral_torque_trajectory(self):
+        """
+        Generate a simple torque trajectory with integral values.
+        """
         torque_points = [0] + random.sample(self.config.torque_steps, k=self.torque_states)
         return self._integral_trajectory(torque_points)
 
     def _gen_integral_speed_trajectory(self):
+        """
+        Generate a simple speed trajectory with integral values.
+        """
         speed_points = [0] + random.sample(self.config.speed_steps, k=self.speed_states)
         return self._integral_trajectory(speed_points)
 
     def _gen_continuous_torque_trajectory(self):
+        """
+        Generate a torque trajectory with continuous values.
+        """
         torque_points = [0] + np.random.uniform(self.config.torque_range[0],
                                                 self.config.torque_range[1],
                                                 self.torque_states)
         return self._continuous_trajectory(torque_points)
 
     def _gen_continuous_speed_trajectory(self):
+        """
+        Generate a speed trajectory with continuous values.
+        """
         speed_points = [0] + np.random.uniform(self.config.speed_range[0],
                                                self.config.speed_range[1],
                                                self.speed_states)
