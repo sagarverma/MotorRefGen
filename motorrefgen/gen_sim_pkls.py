c7b9ff21d686f7e852555053528c819d74ab9778
Sagar <sagar15056@iiitd.ac.in>
2020-04-18 10:15:03 +0200

    gen sim pkl with passed data acq rate

--- motorrefgen/gen_sim_pkls.py
+++ motorrefgen/gen_sim_pkls.py
@@ -1,8 +1,6 @@
 import os
 import argparse
-import random
 
-import numpy as np
 import pickle as pkl
 
 from motorrefgen.experiment import Experiment
@@ -11,35 +9,78 @@ from motorrefgen.config import ExperimentConfig
 from motorsim.simconfig import SimConfig
 from motorsim.simulators.conn_python import Py2Mat
 
+
 def get_arg_parse():
-    parser = argparse.ArgumentParser(description='Generate training and validation data.')
+    """Prase command line arguments.
+
+    Returns
+    -------
+    Namespace
+        ArgumentParser parsed.
+
+    """
+    parser = argparse.ArgumentParser(
+                    description="""Generate training and validation data.""")
     parser.add_argument('--save_dir', required=True, type=str)
     parser.add_argument('--set', type=str, required=True, help='train/val')
-    parser.add_argument('--samples', type=int, required=True, help='Number of samples.')
+    parser.add_argument('--sim_rate', type=float, required=True,
+                        help='Simulation rate')
+    parser.add_argument('--samples', type=int, required=True,
+                        help='Number of samples.')
 
     args = parser.parse_args()
     return args
 
+
 def generate(sim_no, opt):
+    """Generate one sample or random trajectory and simulate.
+
+    Parameters
+    ----------
+    sim_no : int
+        Simulation number in the set.
+    opt : args
+        Parsed arguments.
+
+    Returns
+    -------
+    None
+
+    """
     config = ExperimentConfig(integral=False, simulate=True)
     experiment = Experiment(config)
 
-    config = SimConfig()
-    py2mat = Py2Mat(config)
+    simconfig = SimConfig()
+    simconfig.set_config_from_json({'Data_Ts': opt.sim_rate})
+    py2mat = Py2Mat(simconfig)
 
     experiment.simulate(simulator=py2mat)
     simulation_data = experiment.get_simulation_data()
 
-    fout = open(os.path.join(opt.save_dir, opt.set, str(sim_no).zfill(5) + '.pkl'), 'wb')
+    fout = open(os.path.join(opt.save_dir,
+                opt.set, str(sim_no).zfill(5) + '.pkl'), 'wb')
     pkl.dump(simulation_data, fout)
     fout.close()
 
+
 def generate_set(opt):
+    """Generate set of randomly generated trajectories.
+
+    Parameters
+    ----------
+    opt : args
+        Passed arguments.
+
+    Returns
+    -------
+    None
+    """
     if not os.path.exists(os.path.join(opt.save_dir, opt.set)):
         os.makedirs(os.path.join(opt.save_dir, opt.set))
 
     for i in range(opt.samples):
         generate(i, opt)
+
 
 opt = get_arg_parse()
 generate_set(opt)
