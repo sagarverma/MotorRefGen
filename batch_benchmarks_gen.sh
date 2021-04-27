c9beeb3d5aebc2059e3dda1e24189c53d7e7a7f6
Sagar <sagar15056@iiitd.ac.in>
2021-04-27 11:35:02 +0200

    batch benchmarks generator

--- batch_benchmarks_gen.sh
+++ batch_benchmarks_gen.sh
@@ -1,36 +1,36 @@
 python motorrefgen/gen_custom_pkls.py \
---save_dir=../../datasets/Data_11052020_exponential_dist_ramps/benchmarks \
+--save_dir=../../datasets/Data_27012021_noisy/benchmarks \
 --speed_time=0,1,2,5 --torque_time=0,1,2,5 \
 --reference_speed=0,0,50,50 --reference_torque=0,0,0,0 \
 --sim_rate=0.004 --benchmark_name=bench1
 
 python motorrefgen/gen_custom_pkls.py \
---save_dir=../../datasets/Data_11052020_exponential_dist_ramps/benchmarks \
+--save_dir=../../datasets/Data_27012021_noisy/benchmarks \
 --speed_time=0,1,2,3,4,5,6,7,8 --torque_time=0,1,2,3,4,5,6,7,8 \
 --reference_speed=0,0,5,5,5,5,5,50,50 --reference_torque=0,0,0,0,50,50,50,50,50 \
 --sim_rate=0.004 --benchmark_name=bench2
 
 python motorrefgen/gen_custom_pkls.py \
---save_dir=../../datasets/Data_11052020_exponential_dist_ramps/benchmarks \
+--save_dir=../../datasets/Data_27012021_noisy/benchmarks \
 --speed_time=0,1,2,3,4,5,6,7,8 --torque_time=0,1,2,3,4,5,6,7,8 \
 --reference_speed=0,0,5,5,5,50,50,-50,-50 --reference_torque=0,0,0,0,50,50,50,50,50 \
 --sim_rate=0.004 --benchmark_name=bench3
 
 python motorrefgen/gen_custom_pkls.py \
---save_dir=../../datasets/Data_11052020_exponential_dist_ramps/benchmarks \
+--save_dir=../../datasets/Data_27012021_noisy/benchmarks \
 --speed_time=0,1,2,3,3.004,5 --torque_time=0,1,2,3,3.004,5 \
 --reference_speed=0,0,25,25,25,25 --reference_torque=0,0,0,0,100,100 \
 --sim_rate=0.004 --benchmark_name=bench4
 
 python motorrefgen/gen_custom_pkls.py \
---save_dir=../../datasets/Data_11052020_exponential_dist_ramps/benchmarks \
+--save_dir=../../datasets/Data_27012021_noisy/benchmarks \
 --speed_time=0,1,1.004,5,55,60 --torque_time=0,1,1.004,5,55,60 \
 --reference_speed=0,0,70,70,-70,-70 --reference_torque=0,0,0,0,0,0 \
 --sim_rate=0.004 --benchmark_name=bench5
 
 
 python motorrefgen/gen_custom_pkls.py \
---save_dir=../../datasets/Data_11052020_exponential_dist_ramps/benchmarks \
+--save_dir=../../datasets/Data_27012021_noisy/benchmarks \
 --speed_time=0,1,1.004,5,55,60 --torque_time=0,1,1.004,5,55,60 \
 --reference_speed=0,0,70,70,-70,-70 --reference_torque=0,0,0,0,0,0 \
 --sim_rate=0.004 --benchmark_name=bench5
